ed66178315f20afdcae460c4c67330e15f7316b2
Mauricio Lombano <mauricio.lombano@gmai.com>
2021-06-15 09:23:53 -0500

    Ajs

--- src/app/access-surveys/surveys-questions/surveys-questions.component.ts
+++ src/app/access-surveys/surveys-questions/surveys-questions.component.ts
@@ -35,7 +35,6 @@ export class SurveysQuestionsComponent implements OnInit {
   }
 
   saveAnswers(indice: number, valuacion: any, goNext?: boolean, doLog?: boolean) {
-    console.log("Texto pregunta ", this.textoPregunta6);
     this.respArr[indice].value = valuacion;
     this.respArr[indice].hasAnswer = true;
     if (goNext) {
